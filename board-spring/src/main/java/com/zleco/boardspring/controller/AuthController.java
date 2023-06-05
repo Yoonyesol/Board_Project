d8056ba084416a5674a9994b4e8d6f003b38680f
Yoonyesol <maniac970430@naver.com>
2023-06-05 21:08:17 +0900

    [BE]SignUpResponseDto 수정

--- board-spring/src/main/java/com/zleco/boardspring/controller/AuthController.java
+++ board-spring/src/main/java/com/zleco/boardspring/controller/AuthController.java
@@ -2,7 +2,6 @@ package com.zleco.boardspring.controller;
 
 import com.zleco.boardspring.dto.ResponseDto;
 import com.zleco.boardspring.dto.SignUpDto;
-import com.zleco.boardspring.dto.SignUpResponseDto;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(originPatterns="http://localhost:3000")
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 public class AuthController {
     @PostMapping("/signUp")
-    public ResponseDto<SignUpResponseDto> signUp(@RequestBody SignUpDto requestBody){
+    public ResponseDto<?> signUp(@RequestBody SignUpDto requestBody){
         System.out.println(requestBody.toString());
         return null;
     }
